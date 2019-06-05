be5741c275712597593c92d5d80a97fcdda0973e
betsybaileyy <betsy.bailey80@gmail.com>
2019-06-05 12:51:56 -0700

    linear search recursive

--- Code/search.py
+++ Code/search.py
@@ -17,8 +17,13 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
+    # implement linear search recursively here
+    if item is not none:
+        if item == array[index]
+        return index
+    else:
+        return linear_search_recursive(array, item, index+1)
+
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
