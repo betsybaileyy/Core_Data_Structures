c4c76b793de79a9cfb05daa6f9fd80f60aebffbc
betsybaileyy <betsy.bailey80@gmail.com>
2019-06-26 16:01:52 -0700

    is leaf, is branch, height

--- Code/binarytree.py
+++ Code/binarytree.py
@@ -15,24 +15,26 @@ class BinaryTreeNode(object):
 
     def is_leaf(self):
         """Return True if this node is a leaf (has no children)."""
-        # TODO: Check if both left child and right child have no value
-        return ... and ...
+        # Check if both left child and right child have no value
+        return self.left is None and self.right is None
 
     def is_branch(self):
         """Return True if this node is a branch (has at least one child)."""
-        # TODO: Check if either left child or right child has a value
-        return ... or ...
+        # Check if either left child or right child has a value
+        return self.right is not None or self.left is not None
 
     def height(self):
         """Return the height of this node (the number of edges on the longest
         downward path from this node to a descendant leaf node).
         TODO: Best and worst case running time: ??? under what conditions?"""
-        # TODO: Check if left child has a value and if so calculate its height
-        ...
-        # TODO: Check if right child has a value and if so calculate its height
-        ...
+        # Check if left child has a value and if so calculate its height
+        if self.left is not None:
+            left_len = self.left.height() + 1
+        # Check if right child has a value and if so calculate its height
+        if self.right is not None:
+            right_len = self.right.height() + 1
         # Return one more than the greater of the left height and right height
-        ...
+        return max(right_len, left_len)
 
 
 class BinarySearchTree(object):
