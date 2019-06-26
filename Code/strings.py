84110e5c3ccc933af8c9656757c0cb051c3adc30
betsybaileyy <betsy.bailey80@gmail.com>
2019-06-26 15:12:43 -0700

    formatting error

--- Code/strings.py
+++ Code/strings.py
@@ -4,7 +4,8 @@ def contains(text, pattern):
     """Return a boolean indicating whether pattern occurs in text.
     Best Case Run Time: 0(1) if at begining of string
     Worst Case: 0(n) if item is not at begining
-    Space complexity: 0(n) because we are relaiant on the size of text"""    assert isinstance(text, str), 'text is not a string: {}'.format(text)
+    Space complexity: 0(n) because we are relaiant on the size of text"""
+    assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # Implement contains here (iteratively and/or recursively)
     index = find_index(text, pattern)
