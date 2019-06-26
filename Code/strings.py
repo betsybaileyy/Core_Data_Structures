9889b180cf5502fbe176fb909467e4096b04ddcc
betsybaileyy <betsy.bailey80@gmail.com>
2019-06-26 15:01:30 -0700

    added time complexity

--- Code/strings.py
+++ Code/strings.py
@@ -1,8 +1,10 @@
 #!python
 
 def contains(text, pattern):
-    """Return a boolean indicating whether pattern occurs in text."""
-    assert isinstance(text, str), 'text is not a string: {}'.format(text)
+    """Return a boolean indicating whether pattern occurs in text.
+    Best Case Run Time: 0(1) if at begining of string
+    Worst Case: 0(n) if item is not at begining
+    Space complexity: 0(n) because we are relaiant on the size of text"""    assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # Implement contains here (iteratively and/or recursively)
     index = find_index(text, pattern)
@@ -14,7 +16,9 @@ def contains(text, pattern):
 
 def find_index(text, pattern, offset=0):
     """Return the starting index of the first occurrence of pattern in text,
-    or None if not found."""
+    or None if not found.
+    Run time: 0(n) we are iterating through each index and checking it against the pattern
+    Space complexity: 0(n) because we are relaiant on the size of text"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # Implement find_index here (iteratively and/or recursively)
@@ -42,7 +46,9 @@ def find_index(text, pattern, offset=0):
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
-    or an empty list if not found."""
+    or an empty list if not found.
+    Run time: 0(n) we are iterating through each index and checking it against the pattern
+    Space complexity: 0(n) because we are relaiant on the size of text"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # Implement find_all_indexes here (iteratively and/or recursively)
