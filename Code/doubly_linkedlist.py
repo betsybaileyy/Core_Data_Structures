1580b2bb287b4fb44457a40f5c440891d63d946c
betsybaileyy <betsy.bailey80@gmail.com>
2019-07-02 19:04:32 -0700

    doubly linked list prepend

--- Code/doubly_linkedlist.py
+++ Code/doubly_linkedlist.py
@@ -7,20 +7,37 @@ class Node(object):
         self.next = None
         self.previous = None
 
+        #self.tail = None
+        #self.head = None
+
     def __repr__(self):
         """Return a string representation of this node."""
         return 'Node({!r})'.format(self.data)
 
 
-class Doubly_LinkedList(object):
-    def append(item):
+class DoublyLinkedList(object):
+    def append(self, item):
         new_node = Node(item)
         self.tail.next = new_node
         new_node.previous = self.tail
         self.tail = new_node
 
-    def prepend():
-        pass
+
+    def prepend(self, item):
+        """Insert the given item at the head of this linked list."""
+        new_node = Node(item)
+
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.previous = new_node
+            self.head = new_node
+
+        self.size += 1
+
+
 
     def find():
         pass
